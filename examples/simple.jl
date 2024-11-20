13c4709f7d1c45790307cf6b7ddc9220d5ef2ae1
Jarvist Moore Frost <jarvist@gmail.com>
2024-11-20 10:56:58 +0000

    Add Title; multicolumns for the words to find.

--- examples/simple.jl
+++ examples/simple.jl
@@ -2,6 +2,9 @@ using WordSearch
 
 board=WordSearch.createEmptyBoard(13)
 
+println("Enter Title:")
+title=readline()
+
 println("Enter words:")
 words=readline() |> split 
 show(words)
@@ -20,9 +23,15 @@ open("wordsearch.tex", "w") do f
     # Document setup
     write(f, "\\documentclass[a4paper]{article}\n")
     write(f, "\\usepackage[margin=2cm]{geometry}\n")
-    write(f, "\\usepackage{tikz}\n\n")
+    write(f, "\\usepackage{tikz}\n")
+    write(f, "\\usepackage{multicol}\n")
     write(f, "\\begin{document}\n\n")
     write(f, "\\centering\n\n")
+    # Print title
+    write(f, "\\begin{huge}\n")
+    write(f, title * "\n")
+    write(f, "\\end{huge}\n\n")
+    write(f, "\\vspace{1cm}\n\n")
 
     # Start TikZ picture
     write(f, "\\begin{tikzpicture}\n")
@@ -46,12 +55,15 @@ open("wordsearch.tex", "w") do f
 
     write(f, "\\end{tikzpicture}\n\n")
 
-    # Print word list preserving case
-    write(f, "\\begin{itemize}\n")
-    for word in words
-        write(f, "\\item $(word)\n")
+    # Print word list in columns
+    write(f, "\\vspace{1cm}\n")  # Add some vertical space
+    write(f, "\\begin{multicols}{3}\\raggedcolumns\n")  # 3 columns, adjust number as needed
+    write(f, "\\begin{itemize}\\setlength{\\itemsep}{0.2cm}\n")
+    for word in sort(words)  # Sort words alphabetically
+        write(f, "\\item \\textbf{$(word)}\n")
     end
-    write(f, "\\end{itemize}\n\n")
+    write(f, "\\end{itemize}\n")
+    write(f, "\\end{multicols}\n\n")
 
     write(f, "\\end{document}\n")
 end
